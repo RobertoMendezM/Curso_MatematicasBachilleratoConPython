b6d006673d88fd3269f379a40ff63dea747c2fc6
Roberto Méndez Méndez <robertomm.es@ciencias.unam.mx>
2024-06-19 13:15:31 -0600

    Create pruebaFibonacci1.py

---
+++ 2.ElementosMatemáticosBásicos/fibonacci/pruebaFibonacci1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Permite ejemplificarla utilidad
+de 
+if __name__ == "__main__":
+
+
+Created on Fri Jun 14  2024
+
+@author: rober
+"""
+
+import fibonacci_haciaAdelnate1 as fhd
+import fibonacci_haciaAdelnate1_v2 as fhd2
+
+
+resultado = fhd.fib(57)
+print(resultado)
+
+
+resultado = fhd2.fib(10)
+print(resultado)
+
+
